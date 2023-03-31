a8d705ac505fdc02e03daf012df0ead878d462a3
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-03-30 23:18:49 -0300

    Add files via upload

--- project1/myapp/models.py
+++ project1/myapp/models.py
@@ -2,11 +2,18 @@ from django.db import models
 
 # Create your models here.
 class Projects(models.Model):
-    name = models.CharField(max_length=200);
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
 
 
 class Task(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField()
     project = models.ForeignKey(Projects, on_delete=models.CASCADE)
+    done = models.BooleanField(default=False)
+
+    def __str__(self) -> str:
+        return self.title + ' - ' + self.project.name
 
