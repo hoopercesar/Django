142c598204864159e940e07efa490487e9dade37
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-03-30 23:19:53 -0300

    Add files via upload

--- project1/myapp/views.py
+++ project1/myapp/views.py
@@ -1,15 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, JsonResponse
+from .models import Projects, Task
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Index Page')
+    title = 'Bienvenido a Django!!'
+    return render(request, 'index.html', {'titulo' : title})
 
+def about(request):
+    return render(request, 'about.html')
 
 def hello(request, username):
    
     return HttpResponse('<h1>Hola, Mundo %s</h1>' % username)
     
 
-def about(request):
-    return HttpResponse('about')
+
+
+def projects(request):
+    projects = list(Projects.objects.values())
+    # return JsonResponse(projects, safe=False)
+    return render(request, 'projects.html', {
+        'proyectos': projects,
+    })
+
+def task(request):
+    # task = get_object_or_404(Task, id=id)
+    task = Task.objects.all()
+    # return HttpResponse('task: %s' % task.title)
+    return render(request, 'task.html', {
+        'tareas': task,
+    })
