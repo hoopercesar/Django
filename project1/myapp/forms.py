547f975e649c9ece586e7bcaae70ff173d8175cf
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-04-04 22:20:42 -0300

    Add files via upload

--- project1/myapp/forms.py
+++ project1/myapp/forms.py
@@ -3,3 +3,8 @@ from django import forms
 class CreateNewTask(forms.Form): 
     title = forms.CharField(label='Titulo Tarea', max_length=200)
     description = forms.CharField(widget=forms.Textarea)
+
+
+class CreateNewProject(forms.Form): 
+    name = forms.CharField(label='Nombre del proyecto', max_length=100)
+    
