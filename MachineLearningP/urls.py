9ad53d1517b2356e4d1d0153db6c6e39a15bc274
Cesar S. Hooper <86272570+hoopercesar@users.noreply.github.com>
2023-05-01 20:52:25 -0400

    Add files via upload

--- MachineLearningP/urls.py
+++ MachineLearningP/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from software import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
-    path('base/', views.base)
+    path('base/', views.base),
+    path('django_plotly_dash/', include('django_plotly_dash.urls')),
 ]
